985f7be1da8bfddd97b5bcf09074550ff871e515
Yang Zhang <zy31415@gmail.com>
2018-05-17 16:06:01 +0800

    Simplify logic.

--- solution/src/main/java/clrs/ch32/e1/q4/WildcardMatcher.java
+++ solution/src/main/java/clrs/ch32/e1/q4/WildcardMatcher.java
@@ -20,39 +20,24 @@ public class WildcardMatcher {
         if (memo[s][t] != -1)
             return memo[s][t] == 1;
 
+        boolean res;
+
         // base case 1: when t == 0;
         if (t == 0) {
-            boolean res = (s == 0);
-            memo[s][t] = res ? 1 : 0;
-            return res;
+            res = (s == 0);
         }
-
-        if (P.charAt(t-1) == '*') {
-            boolean res = false;
-
+        else if (P.charAt(t-1) == '*') {
+            res = false;
             for (int i = s; i >= 0; i--) {
                 res = res || isMatch(T, i, P, t-1);
             }
-            memo[s][t] = res ? 1 : 0;
-            return res;
+        }
+        else {
+            res = s != 0 && P.charAt(t-1) == T.charAt(s-1) && isMatch(T, s-1, P, t-1);
         }
 
-        // base case 2: when s == 0 but t > 0 and P[t-1] != *
-        if (s == 0) {
-            memo[s][t] = 0;
-            return false;
-        }
-
-        if (P.charAt(t-1) != T.charAt(s-1)){
-            memo[s][t] = 0;
-            return false;
-        }
-
-
-        boolean res = isMatch(T, s-1, P, t-1);
         memo[s][t] = res ? 1 : 0;
         return res;
-
     }
 
     static void initMemo(int n, int m) {
